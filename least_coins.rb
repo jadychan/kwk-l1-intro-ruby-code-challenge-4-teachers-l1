01f60cb76b881e19faa99372076aa208885dbf70
jadychan <jadychan123@gmail.com>
2018-06-06 17:23:32 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,28 @@
 #write out your code here
 
 def least_coins(cents)
-
+  type = {
+    :quarters => 0,
+    :dimes => 0,
+    :nickels => 0,
+    :pennies => 0
+  }
 #Code your answer here!
-
+  while cents != 0
+    if cents >= 25
+      type[:quarters] += 1
+      cents -= 25
+    elsif cents >= 10 && cents < 25
+      type[:dimes] += 1
+      cents -= 10
+    elsif cents >= 5 && cents < 10
+      type[:nickels] += 1
+      cents -= 5
+    elsif cents >= 1 && cents < 5
+      type[:pennies] += 1
+      cents -= 1
+    end
+  end
+  return type
 end
+least_coins(29)
