60c9a4ab8f3e9a02d5f0b2d6749f68e961da4db5
jadychan <jadychan123@gmail.com>
2018-06-06 17:21:46 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -12,6 +12,9 @@ def least_coins(cents)
     if cents >= 25
       type[:quarters] += 1
       cents -= 25
+    elsif cents >= 10 && cents < 25
+      type[:dimes] += 1
+      cents -= 10
     end
   end
   puts type
