c84d379def195f8b7f305a7b17f732e913369518
jadychan <jadychan123@gmail.com>
2018-06-06 17:20:15 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,9 +1,9 @@
 #write out your code here
 type = {
-  :quarters = 0,
-  :dimes = 0,
-  :nickels = 0,
-  :pennies = 0
+  :quarters => 0,
+  :dimes => 0,
+  :nickels => 0,
+  :pennies => 0
 }
 
 def least_coins(cents)
