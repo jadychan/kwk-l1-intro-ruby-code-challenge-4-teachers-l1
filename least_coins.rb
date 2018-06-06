e180272105cbf70877453b9d6e697dc3f30b2f0c
jadychan <jadychan123@gmail.com>
2018-06-06 17:20:40 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,17 +1,16 @@
 #write out your code here
-type = {
-  :quarters => 0,
-  :dimes => 0,
-  :nickels => 0,
-  :pennies => 0
-}
 
 def least_coins(cents)
-
+  type = {
+    :quarters => 0,
+    :dimes => 0,
+    :nickels => 0,
+    :pennies => 0
+  }
 #Code your answer here!
   while cents != 0
     if cents >= 25
-      type[:quarters] => 1
+      type[:quarters] += 1
       cents -= 25
     end
   end
